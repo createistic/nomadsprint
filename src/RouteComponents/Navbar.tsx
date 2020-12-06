cc7613bed2bb3ad84a2b075fb2d4c5460918b309
Reece Cunningham <reece.cunningham.dev@outlook.com>
2020-12-06 14:43:44 +0000

    feat(navigation): nav bar added for pages and responsive to screen sizes

--- src/RouteComponents/Navbar.tsx
+++ src/RouteComponents/Navbar.tsx
@@ -1,15 +1,30 @@
 import * as React from "react";
 import "../styles/navbar.css";
+import { Link } from "react-router-dom";
 
 const NavBar: React.FC = (): React.ReactElement => {
   return (
-    <div>
-      <nav>
-        <li>Logo</li>
-        <li>Bussiness</li>
-        <li>overview</li>
-      </nav>
-    </div>
+    <nav>
+      <h1>
+        <Link className="logo" to="/">
+          Logo
+        </Link>
+      </h1>
+      <ul>
+        <Link to="/Business">
+          <li>Business</li>
+        </Link>
+        <Link to="/Overview">
+          <li>Overview</li>
+        </Link>
+        <Link to="/Tech">
+          <li>Tech</li>
+        </Link>
+        <Link to="/UX">
+          <li>UX</li>
+        </Link>
+      </ul>
+    </nav>
   );
 };
 
