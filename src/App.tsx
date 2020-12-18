eb14d35750148d30516d70236648e9c3a3132799
Shaun-Farrell <shaunfarrell@outlook.com>
2020-12-18 16:48:58 +0000

    fix: test again 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./components/About";
 import Profile from "./components/Profile";
 import NewEvent from "./components/NewEvent";
